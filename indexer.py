9d9c9f3ef473a90f1863a8ffead20c7b69b161cf
Matúš Ján Haľama <55839292+MatHal-STU@users.noreply.github.com>
2023-11-13 17:50:39 +0100

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -1,5 +1,15 @@
 import lucene
 
+from java.nio.file import Paths
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.document import Document, Field, FieldType, TextField
+from org.apache.lucene.index import IndexOptions, IndexWriter, IndexWriterConfig, DirectoryReader, Term
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.store import MMapDirectory
+from org.apache.lucene.search import IndexSearcher
+
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
 print("hello")
 
 print(lucene.VERSION)
