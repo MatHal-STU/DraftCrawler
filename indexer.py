781c4a26ac34ee442a96ce562fc687c660880870
Matúš Ján Haľama <55839292+MatHal-STU@users.noreply.github.com>
2023-11-25 17:00:38 +0100

    pylucene

--- indexer.py
+++ indexer.py
@@ -8,9 +8,11 @@ from org.apache.lucene.index import IndexOptions, IndexWriter, IndexWriterConfig
 from org.apache.lucene.queryparser.classic import QueryParser
 from org.apache.lucene.store import MMapDirectory
 from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.search import BooleanQuery, BooleanClause, TermQuery, TermRangeQuery
 
 
 lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
 
 def index_csv_data(csv_data, index_writer):
     for row in csv_data:
@@ -20,7 +22,10 @@ def index_csv_data(csv_data, index_writer):
         doc.add(StringField("round", row[2], Field.Store.YES))
         doc.add(StringField("overall", row[3], Field.Store.YES))
         doc.add(StringField("year", row[4], Field.Store.YES))
+        doc.add(StringField("position", row[5], Field.Store.YES))
+        doc.add(StringField("nationality", row[6], Field.Store.YES))
         index_writer.addDocument(doc)
+
 
 with open("output.csv", "r", newline="", encoding="utf-8") as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
@@ -33,13 +38,51 @@ with open("output.csv", "r", newline="", encoding="utf-8") as csv_file:
     writer.commit()
     writer.close()
 
+user_name = ""
+user_position = ""
+user_nationality = "Sk"
+user_round = ""
+user_overall = "1st"
+start_year = ""
+end_year = ""
+
 searcher = IndexSearcher(DirectoryReader.open(store))
-query_parser = QueryParser("round", analyzer)
-query = query_parser.parse("1st")
+boolean_query = BooleanQuery.Builder()
 
-results = searcher.search(query, 20)  # Adjust the number of results as needed
+if user_name:
+    name_term = Term("name", user_name)
+    name_query = TermQuery(name_term)
+    boolean_query.add(name_query, BooleanClause.Occur.MUST)
 
+if user_position:
+    position_term = Term("position", user_position)
+    position_query = TermQuery(position_term)
+    boolean_query.add(position_query, BooleanClause.Occur.MUST)
+
+if user_nationality:
+    nationality_term = Term("nationality", user_nationality)
+    nationality_query = TermQuery(nationality_term)
+    boolean_query.add(nationality_query, BooleanClause.Occur.MUST)
+
+if start_year and end_year:
+    year_range_query = TermRangeQuery.newStringRange("year", start_year, end_year, True, True)
+    boolean_query.add(year_range_query, BooleanClause.Occur.MUST)
+
+if user_round:
+    round_term = Term("round", user_round)
+    round_query = TermQuery(round_term)
+    boolean_query.add(round_query, BooleanClause.Occur.MUST)
+
+if user_overall:
+    overall_term = Term("overall", user_overall)
+    overall_query = TermQuery(overall_term)
+    boolean_query.add(overall_query, BooleanClause.Occur.MUST)
+
+results = searcher.search(boolean_query.build(), 10)  # Adjust the number of results as needed
 # Print the matching entries
 for score_doc in results.scoreDocs:
     doc = searcher.doc(score_doc.doc)
-    print("Name: {}, Team: {}, Year: {}".format(doc.get("name"), doc.get("team"), doc.get("year")))
+    print("Name: {}, Team: {}, Year: {}, Round: {}, Overall: {}, Position: {}, Nationality: {}".format(
+        doc.get("name"), doc.get("team"), doc.get("year"), doc.get("round"), doc.get("overall"), doc.get("position"),
+        doc.get("nationality")
+    ))
--- regex.py
+++ regex.py
@@ -8,15 +8,16 @@ from bs4 import BeautifulSoup
 # draft_overall = re.search('<strong>Draft<\/strong>: <a href="(\/)teams\/[^"]+\/draft.html">(.*?)<\/a>, (.*?) round \((.*?) overall\),', soup).group(4).strip()
 # draft_year = re.search('<a href="\/draft\/NHL_(\d{4})_entry.html">(.*?)<\/a>', soup).group(1).strip() 
 #  re.search('<p><strong>Team Names:<\/strong>(.*?)<\/p>', html):
+i= 0
 
-
-with open('html.txt', 'r', encoding='utf-8') as file:
+with open('html_final.txt', 'r', encoding='utf-8') as file:
     a=file.read()
     pages = a.split("</html>")
-
 f = open('output.csv', 'a', encoding='utf-8', newline="")
 out = csv.writer(f, delimiter=",")
 for page in pages:
+    i += 1
+    print(i)
     if not re.search('<h1> <span>(.*?)<\/span> <\/h1>', page):
         continue
     name = re.search('<h1> <span>(.*?)<\/span> <\/h1>', page).group(1).strip()
@@ -25,7 +26,12 @@ for page in pages:
     draft_round = re.search('<strong>Draft<\/strong>: <a href="(\/)teams\/[^"]+\/draft.html">(.*?)<\/a>, (.*?) round \((.*?) overall\),', page).group(3).strip()
     draft_overall = re.search('<strong>Draft<\/strong>: <a href="(\/)teams\/[^"]+\/draft.html">(.*?)<\/a>, (.*?) round \((.*?) overall\),', page).group(4).strip()
     draft_year = re.search('<a href="\/draft\/NHL_(\d{4})_entry.html">(.*?)<\/a>', page).group(1).strip() 
-    out.writerow([name, draft_team, draft_round, draft_overall,draft_year])
+    position = re.search('<strong>Position<\/strong>: (.*?) (.*?);', page).group(1).strip() 
+    if not re.search('<span class="(.*?)" style="">(.*?)<\/span>', page):
+        nationality = 'Unknown'
+    else:
+        nationality = re.search('<span class="(.*?)" style="">(.*?)<\/span>', page).group(2).strip().capitalize() 
+    out.writerow([name, draft_team, draft_round, draft_overall, draft_year, position,nationality])
 
 f.close()
 
